fd3f7a83908e3288d42b29557b36e65b8f1fcb41
nishNSUT <153387584+nishNSUT@users.noreply.github.com>
2024-03-16 01:00:31 +0530

    added randomTexts to support dynamic texts

---
+++ randomTexts.js
@@ -0,0 +1,52 @@
+let user = "prachi";
+let storageList = localStorage.getItem(user);
+let list = (storageList == null) ? [] : JSON.parse(storageList);
+
+const editItem = (index) => {
+    let newTask = prompt("Enter Updated Text", list[index][0]);
+    while (newTask === "") {
+        newTask = prompt("Enter Updated Text (Can't be Empty)", list[index][0]);
+    }
+    list[index][0] = newTask;
+    localStorage.setItem(user, JSON.stringify(list));
+    update();
+}
+const deleteItem = (index) => {
+    list.splice(index, 1);
+    localStorage.setItem(user, JSON.stringify(list));
+    update();
+}
+const tickItem = (index) => {
+    list[index][1] = !list[index][1];
+    localStorage.setItem(user, JSON.stringify(list));
+    update();
+}
+const update = () => {
+    let strHTML = '';
+    list.forEach((element, index) => {
+        if (element[1]) strHTML += '<tr><td class="col1">' + (index + 1) + '</td><td class="col2">' + (element[0]) + '</td><td class="col3"><button onclick = tickItem(' + index + ') title="Disable"><i class="fa fa-times"></i></button><button onclick = editItem(' + index + ') title="Edit"><i class="fa fa-edit" ></i></button><button onclick = deleteItem(' + index + ') title="Delete"><i class="fa fa-trash"></i></button></td></tr>';
+        else strHTML += '<tr><td class="col1">' + (index + 1) + '</td><td class="col2"><s>' + (element[0]) + '</s></td><td class="col3"><button onclick = tickItem(' + index + ') title="Enable"><i class="fa fa-check"></i></button><button onclick = editItem(' + index + ') title="Edit"><i class="fa fa-edit" ></i></button><button onclick = deleteItem(' + index + ') title="Delete"><i class="fa fa-trash"></i></button></td></tr>';
+    });
+    document.getElementById('tableBody').innerHTML = strHTML;
+
+}
+const addItem = () => {
+    let task = document.getElementById('task').value;
+    if (task === '') {
+        alert("Please Enter Text");
+    }
+    else {
+        list.push([task, true]);
+        localStorage.setItem(user, JSON.stringify(list));
+        update();
+        document.getElementById('task').value = '';
+    }
+}
+const clearAll = () => {
+    if (confirm("Do you want to clear all?")) {
+        list = [];
+        localStorage.setItem(user, JSON.stringify(list));
+        update();
+    }
+}
+update();
